b5807780b496c24e0b87f3d0977e59dd39235392
Julian Mateo Guzman Alba <ju.guzman@udla.edu.co>
2024-06-13 10:27:52 -0500

    feat: Esquema inicial del proyecto

---
+++ Assets/Scripts/ScriptableObjects/SO_Inputs.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_Inputs", menuName = "ScriptableObjects/Input Manager")]
+public class SO_Inputs : ScriptableObject
+{
+
+}
